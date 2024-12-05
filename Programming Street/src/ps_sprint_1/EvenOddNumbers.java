8ef71b732b31950e741fc629e26abfc0d77079a2
pbtech25 <pbtech25@gmail.com>
2024-12-05 19:46:54 +0530

    Solved Even Odd checking Problem

--- Programming Street/src/ps_sprint_1/EvenOddNumbers.java
+++ Programming Street/src/ps_sprint_1/EvenOddNumbers.java
@@ -1,10 +1,21 @@
 package ps_sprint_1;
+import java.util.*;
 
 public class EvenOddNumbers {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.println("Enter the number: ");
+		int n = sc.nextInt();
+		
+		if(n%2==0) {
+			System.out.println("The number is even!");
+		}else {
+			System.out.println("The number is Odd!");
+		}
+		
+		sc.close();
 	}
 
 }
